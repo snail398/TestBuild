2e2159aef69822a27cfa5dac085c96b84041ab75
Roma Shtykov <rshtykov@skazbuka.com>
2020-01-13 14:22:25 +0300

    first commit

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,29 @@
+﻿using Boo.Lang;
+using UnityEditor;
+
+public class BuildScript
+{
+    static string[] EnabledScenes = FindEnabledEditorScenes();
+    
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions
+        {
+            scenes = EnabledScenes,
+            locationPathName = "./builds/game.x86_64.exe",
+            target = BuildTarget.StandaloneWindows,
+            options = BuildOptions.None
+        });
+    }
+    
+    private static string[] FindEnabledEditorScenes(){
+ 
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes){
+            if (scene.enabled){
+                EditorScenes.Add(scene.path);
+            }
+        }
+        return EditorScenes.ToArray();
+    }
+}
